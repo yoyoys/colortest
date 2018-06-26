53dab28d42db58a45c4557c9bcec338b2244a7ef
Yoyo Young <yoyoys@livemail.tw>
2018-06-27 00:13:13 +0800

    🎉 release

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+namespace colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            var text = "***";
+            var colors = Enum.GetValues(typeof(ConsoleColor)).Cast<ConsoleColor>().ToList();
+            var darkColors = colors.Take(8);
+            var lightColors = colors.Skip(8);
+            
+            Console.Write(new String(' ', 14));
+
+            foreach (var color in darkColors) {
+                var colorName = color.ToString().Take(7);
+                Console.Write(new string(colorName.ToArray()).PadRight(7));
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
+            foreach (var fore in colors)
+            {
+                Console.Write(fore.ToString().PadLeft(13));
+                Console.Write(' ');
+                foreach (var back in darkColors)
+                {
+                    Console.ForegroundColor = fore;
+                    Console.BackgroundColor = back;
+                    Console.Write($" {text} ");
+
+                    Console.ResetColor();
+                    Console.Write("   ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.Write(new String(' ', 14));
+
+            foreach (var color in lightColors) {
+                var colorName = color.ToString().Take(7);
+                Console.Write(new string(colorName.ToArray()).PadRight(7));
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
+
+            foreach (var fore in colors)
+            {
+                Console.Write(fore.ToString().PadLeft(13));
+                Console.Write(' ');
+                foreach (var back in lightColors)
+                {
+                    Console.ForegroundColor = fore;
+                    Console.BackgroundColor = back;
+                    Console.Write($" {text} ");
+
+                    Console.ResetColor();
+                    Console.Write("   ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# colortest
+
+Simply install with `dotnet tool install -g colortest `   
+then run `colortest`
+
+# For Mac Users with zsh
+This tool is made by my MacBook.  
+And I noticed that I can't run with zsh by default.  
+You'll need add `$HOME/.dotnet/tools` to your `.zshrc` file like:  
+```
+export PATH="$PATH:$HOME/.dotnet/tools"
+```
